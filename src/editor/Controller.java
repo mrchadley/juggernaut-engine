ad33bb82131a00f70b9ba75ccb3edbd2971755c4
mrchadley <marc.tremblay@uoit.net>
2016-02-12 00:20:42 -0500

    prepping to work on engine source -marc

---
+++ src/editor/Controller.java
@@ -0,0 +1,13 @@
+package editor;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+public class Controller
+{
+
+}
---
+++ src/engine/AbstractGame.java
@@ -0,0 +1,7 @@
+package engine;
+
+/**
+ * Created by marct on 2016-02-12.
+ */
+public class AbstractGame {
+}
