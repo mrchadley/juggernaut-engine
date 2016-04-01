2c774ab8498fee3748714973595f27eed5f61faf
mrchadley <marc.tremblay@uoit.net>
2016-04-01 15:34:15 -0400

    working on the chat part... still -marc

--- src/editor/J_ChatController.java
+++ src/editor/J_ChatController.java
@@ -1,12 +1,41 @@
 package editor;
 
-/**
- * Created by 100555250 on 3/31/2016.
- */
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 public class J_ChatController
 {
+    public String name;
+    public String ip;
+
+    @FXML
+    TextField textField;
+    @FXML
+    TextArea textArea;
+
     public void initialize()
     {
+        textArea.setWrapText(true);
+        textField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCode() == KeyCode.ENTER)
+                    SendMessage(null);
+            }
+        });
+    }
 
+    public void SendMessage(ActionEvent actionEvent)
+    {
+        if(textField.getText().intern() != "")
+        {
+            textArea.setText(textArea.getText() + name.toUpperCase() + ": " + textField.getText() + "\n");
+            textField.clear();
+        }
     }
 }
